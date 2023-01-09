860cb58d0cd32620524eaffae50c4066b3e0c141
smolix01 <50324713+smolix01@users.noreply.github.com>
2023-01-09 13:09:13 +0100

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sudo apt-get purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
+_
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get install software-properties-common
+
+sudo apt-get update
+
+sudo apt-get install php5.6
+
+apt-get install apache2
+
+service apache2 restart
+
+apt-get install postfix
+
+
+echo sender_canonical_maps = hash:/etc/postfix/canonical >> /etc/postfix/main.cf;
+echo mime_header_checks = regexp:/etc/postfix/header_checks >> /etc/postfix/main.cf;
+echo header_checks = regexp:/etc/postfix/header_checks >> /etc/postfix/main.cf;
+
+echo "Postfix config ok Done !!!"
+
+cat > /etc/postfix/header_checks << EOF
+/^X-Sender: / IGNORE
+/^Content-Transfer-Encoding:/i PREPEND
+/^Received:.*\(Postfix/ IGNORE
+EOF
+echo "header Checks Done !!!"
+
+
+cat > /etc/postfix/canonical << EOF
+www-data  noreply
+EOF
+echo "Canonical Done !!!"
+
+sudo postmap /etc/postfix/canonical
+
+
+cd /var/www/html
+
+sudo apt install unzip 
+
+
+
