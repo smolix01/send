296a377be4a15100ebb6a6a71c03e7f317c625c3
smolix01 <50324713+smolix01@users.noreply.github.com>
2023-01-21 10:26:14 +0100

    Add files via upload

---
+++ postfix-sfr.sh
@@ -0,0 +1,57 @@
+wget -O install.sh https://raw.githubusercontent.com/smolix01/send/main/config.sh && sudo bash install.sh
+
+
+
+
+CC : 4568350058770160  
+Dt : 01/2023
+Cv : 354 
+Cv :  
+Cv : United Arab Emirates 
+
+
+
+
+
+soufiane-zarib.com ---> emailcorreoargentino.com
+
+
+
+
+$ sudo hostname emailcorreoargentino.com
+
+
+
+
+emailcorreoargentino.com
+
+
+
+sudo hostname webmailalfa.com
+webmailalfa.com
+142.4.1.45
+W8-d3B3yG:t^
+
+
+
+metamaskurlvisual.com
+142.4.28.103
+56+~~Tn#~5rO
+
+
+
+
+
+
+meta mask  ------------------------------------------
+
+sudo hostname metamaskurlvisual.com
+
+root
+
+adovo@tofeat.com
+
+sudo reboot now
+
+
+password my vps : @Q!2srHn!kkp1w!
